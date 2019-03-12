9e5bf36be69a1d04480a01ab3664f663e7452f06
EC2 Default User <ec2-user@ip-172-31-36-175.us-east-2.compute.internal>
2019-03-12 21:23:58 +0000

    Bug Fix

--- spec/controllers/admin/categories_controller_spec.rb
+++ spec/controllers/admin/categories_controller_spec.rb
@@ -34,6 +34,25 @@ describe Admin::CategoriesController do
     end
   end
 
+# Start of new rspec test cases
+# New Rspec Test for create 
+  it "test_create" do
+    post :new, :category => { name: 'new category test', keywords: 'test', permalink: 'test', description: 'test' }
+    assert_response :redirect, :action => 'index'
+    new_category = Category.find_by_name("new category test")
+    assert_not_nil new_category
+  end
+
+# New Rspec test to edit an existing category 
+  it 'should edit an existing category' do
+     post :edit, :category =>{:name => "new category test", :keywords => "test", :permalink => "test", :description => "test"}
+    assert_response :redirect, :action => "index"
+    assert_not_nil assigns(:category)
+    expect(flash[:notice]).to eq("Category was successfully saved.")
+  end
+
+# End of new rspec tests
+
   it "test_update" do
     post :edit, :id => Factory(:category).id
     assert_response :redirect, :action => 'index'
